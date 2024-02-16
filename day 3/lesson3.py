8678bd9f840e23f23de0cda1fd3d80682505a15b
giorgikaplanishvili <giokaflanishvil@gmail.com>
2024-02-17 01:09:28 +0400

    third commit

---
+++ day 3/lesson3.py
@@ -0,0 +1,13 @@
+age_dad = 36
+age_mom = 36
+age_sister = 16
+age_sister2 = 9
+print("dad" + str(age_dad))
+print("mom" + str(age_mom))
+print("sister" + str(age_sister))
+print("sister2" + str(age_sister))
+
+print("dad in 2 years" + str(age_dad + 2))
+print("mom in 2 years" + str(age_mom + 2))
+print("sister in 2 years" + str(age_sister + 2))
+print("sister2 in 2years" + str(age_sister2 + 2))
