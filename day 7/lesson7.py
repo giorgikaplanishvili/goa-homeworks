f451dcc03095fad4bb43131a4c166a42d2ccc1d8
giorgikaplanishvili <giokaflanishvil@gmail.com>
2024-03-06 01:11:14 +0400

    seventh

---
+++ day 7/lesson7.py
@@ -0,0 +1,29 @@
+# 1.
+
+for i in range(0,10,2):
+    print(i)
+
+ # 2.
+
+i = 1 
+
+while i < 20:
+    print(i)
+    i = i + 1
+
+# 3.
+
+result = 0
+i = 0
+
+while i <= 5:
+     result = result + i
+     i = i + 1
+
+print(result)
+
+# 4.
+
+name = "Giorgi"
+for i in name:
+     print(i)
