3e6ae0f6b0fb0ad30a7f504f8d12d476e4cca7c3
giorgikaplanishvili <giokaflanishvil@gmail.com>
2024-03-20 14:30:49 +0400

    twelfth commit

---
+++ day 12/lesson12.py
@@ -0,0 +1,7 @@
+list = ['Schindlers List','Batman: Mask of the Phantasm','Star Wars: Revenge of the Sith','Juice Wrld: Into the Abyss','BoJack Horseman']
+print(list[0:2])
+print(list[2:4])
+print(list[1:4])
+print(list[-5:-3])
+print(list[-3:-1])
+print(list[-4:-1])
