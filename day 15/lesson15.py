f194f3f27e5c99f73046c0ba31f2cf2b2f27dcaa
giorgikaplanishvili <giokaflanishvil@gmail.com>
2024-03-29 02:11:12 +0400

    fifteenth commit

---
+++ day 15/lesson15.py
@@ -0,0 +1,22 @@
+def print_name(name):
+        print("hello" + name)
+
+
+print_name("gio")
+print_name("luka")
+print_name("saba")
+
+
+def add_numbers(num1, num2):
+        print(num1 + num2)
+
+add_numbers(5, 15)
+add_numbers(17, 72)
+add_numbers(87, 29)
+
+def multiplication(num1, num2):
+        print(num1 * num2)
+
+multiplication(12,29)
+multiplication(17, 2)
+multiplication(9, 15)
